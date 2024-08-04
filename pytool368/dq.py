0d79e4af7d68479017270fa3530a195886f4b49e
miyanew <dev38_shin@yahoo.co.jp>
2024-08-04 23:30:27 +0900

    ダブルクオートで囲うコマンドをつくった

---
+++ pytool368/dq.py
@@ -0,0 +1,31 @@
+#! /home/ubu/.pyenv/shims/python3
+
+import argparse
+import sys
+
+
+def main(options):
+    if options["list"] is None:
+        inp = sys.stdin.read()
+    else:
+        inp = options["list"]
+
+    if options["d"] is None:
+        delim = ", "
+    else:
+        delim = options["d"]
+
+    splitted = inp.strip().split()
+    enclosed = [f'"{s}"' for s in splitted]
+    joined = f"{delim}".join(enclosed)
+    print(joined)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("list", nargs='?', help="スペース区切りの文字列リスト")
+    parser.add_argument("-d", nargs='?', help="区切り文字")
+    args = parser.parse_args()
+    arguments = vars(args)
+
+main(arguments)
