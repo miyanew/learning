2663580ad14eba9ce8e3fe2983c0a0e25ef42a75
miyanew <dev38_shin@yahoo.co.jp>
2024-10-04 02:18:58 +0900

    S3からSSHサーバへのファイル転送

---
+++ WinFt/src/S3toServer.ps1
@@ -0,0 +1,12 @@
+Set-Location $PSScriptRoot
+
+& .\Ls3.ps1
+Write-Output "---"
+
+& .\GetS3.ps1 $args[0]
+Write-Output "File retrieval complete, starting transfer."
+
+& .\PutRemote.ps1 $args[0] $args[1]
+Write-Output "File transfer complete!`n---"
+
+& .\LsRemote.ps1 $args[0]
